9b77758b6ef9a6adfb62570530b9cab438ae4e69
Ishwar Jagdale <ishwarrules@gmail.com>
2023-01-12 16:15:13 +0530

    possible heroku fix for procfile:

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+import os
+
 from flask import Flask
 from database import db
 from auth import auth, login_manager
@@ -26,4 +28,4 @@ def hello_world():  # put application's code here
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(host='0.0.0.0', port=os.environ.get("PORT") or 5000)
