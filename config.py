cfcb5733d0874498cbf816bc139465d3919ccd92
Ishwar Jagdale <ishwarrules@gmail.com>
2023-01-12 15:43:57 +0530

    initial commit

--- config.py
+++ config.py
@@ -2,9 +2,6 @@ import datetime
 from os import environ
 FLASK_ENV = environ.get("FLASK_ENV")
 SQLALCHEMY_DATABASE_URI = 'postgresql://scapp:password@localhost:5432/scapp'
-# SQLALCHEMY_DATABASE_URI =
-# 'postgresql://roflqvnohnziil:d4a10383c52883ababaaf29570153a1594db0b79fc0471156d9112a00e103f3b@ec2-35-170-21-76
-# .compute-1.amazonaws.com:5432/d70oqtafiofkov'
 FLASK_DEBUG = environ.get("FLASK_DEBUG") or False
 SECRET_KEY = environ.get("SECRET_KEY")
 SESSION_COOKIE_SAMESITE = "None"
