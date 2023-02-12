27cfdab3f913413119aeb89f13f0d3d2126f9e60
Ishwar Jagdale <ishwarrules@gmail.com>
2023-02-12 06:45:50 +0530

    image to be deleted is taken from args

--- api/admin.py
+++ api/admin.py
@@ -101,7 +101,7 @@ def delete_image(monument_id):
         return jsonify({"monument_id": monument.monument_id, "message": "image uploaded!"})
 
     if request.method == "DELETE":
-        image = MonumentImages.query.filter_by(monument_id=monument_id, image=request.json['image']).first()
+        image = MonumentImages.query.filter_by(monument_id=monument_id, image=request.args['image']).first()
 
         if image:
             deleteBlob(image.image)
